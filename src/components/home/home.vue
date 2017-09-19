ddf3f157cb6ed0d409c7aeedf3685908a901ac69
liuliu <LIULIU762@pingan.com.cn>
2017-09-19 10:54:46 +0800

    自己写的头部与小虫头部对比学习笔记

---
+++ src/components/home/home.vue
@@ -0,0 +1,63 @@
+<template>
+    <section>
+        <titleBar></titleBar>
+    </section>
+</template>
+<script>
+
+import titleBar from '../titlebar/main-titlebar';
+import { mapState } from 'vuex';
+let i = 1;
+const message = ['hello world', 'hello'];
+
+export default {
+    components: {
+        titleBar
+    },
+    props: {
+        prop: {
+            type: String,
+            default: 'hello'
+        }
+    },
+    data () {
+        return {
+            key: 'ok'
+        };
+    },
+    computed: {
+        ...mapState({
+            message: state => state.appState.message
+        }),
+        ckey () {
+            return this.key + 'yeah';
+        }
+    },
+    methods: {
+        onButtonClick () {
+            i = i === 0 ? 1 : 0;
+            this.$store.dispatch('initStatus', message[i]);
+        }
+    },
+    created () {
+    },
+    mounted () {
+    }
+};
+
+</script>
+
+<style scoped>
+    .demo {
+        margin-top: 20px;
+    }
+
+    .button {
+        width: 200px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+        background: #ccc;
+        border-radius: 15px;
+    }
+</style>
